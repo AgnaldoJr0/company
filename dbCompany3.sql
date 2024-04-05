3d056fa5297c4f2b95018de031d1585a3e101c53
AgnaldoJr0 <113722348+AgnaldoJr0@users.noreply.github.com>
2024-04-05 02:45:32 -0300

    Update dbCompany3.sql

--- dbCompany3.sql
+++ dbCompany3.sql
@@ -1,5 +1,36 @@
 USE company_database;
 
+SELECT Dname, COUNT(*) AS num_people
+FROM employee e
+JOIN department d ON e.Dno = d.Dnumber
+GROUP BY Dname
+ORDER BY num_people DESC
+LIMIT 2;
+
+-- Índice utilizado: idx_employee_Dno
+-- Tipo de índice: Índice B-tree
+-- Motivo da escolha: Pela Eficiência de busca e Ordenação.
+CREATE INDEX idx_employee_Dno ON employee(ativo) USING btree;
+
+SELECT Dname, Dlocation
+FROM department d
+JOIN dept_locations dl ON d.Dnumber = dl.Dnumber;
+
+-- Índice utilizado: idx_dept_locations_Dnumber
+-- Tipo de índice: Índice B-tree
+-- Motivo da escolha: Pela mesma razão do índice acima mas, também por conta à sua utilização em operações de junção.
+CREATE INDEX idx_dept_locations_Dnumber ON dept_locations(ativo) USING btree;
+
+SELECT Dname, COUNT(*) AS num_employees
+FROM employee e
+JOIN department d ON e.Dno = d.Dnumber
+GROUP BY Dname;
+
+-- Índice utilizado: idx_employee_Dno
+-- Tipo de índice: Índice B-tree
+-- Motivo da escolha: O índice B-tree ajuda a acelerar a busca, aumentando assim a eficiência da pesquisa e acelerando a junção da coluna Dno com a tabela department.
+CREATE INDEX idx_employee_Dno ON employee(ativo) USING btree;
+
 DELIMITER \\
 CREATE PROCEDURE manipulate_data(
     IN action INT,
@@ -32,40 +63,18 @@ BEGIN
 END\\
 DELIMITER ;
 
-CALL manipulate_data(1, NULL, 'Joana', 'M', 'Silva', '555666777', '1990-07-15', 'Rua das Flores, 123', 'F', 55000.00, NULL, 2);
+CALL manipulate_data(1, 3, 'Joana', 'M', 'Silva', '555666777', '1990-07-15', 'Rua das Flores, 123', 'F', 55000.00, '351453829', 2);
 -- Para INSERT, onde o segundo parâmetro é o ID do funcionário que você deseja inserir
 
 CALL manipulate_data(2, 3, 'Joana', 'M', 'Oliveira', '555666777', '1990-07-15', 'Avenida das Palmeiras, 456', 'F', 60000.00, '123456789', 1);
 -- Para UPDATE, onde o segundo parâmetro é o ID do funcionário que você deseja atualizar e os demais parâmetros são os novos valores
 
-CALL manipulate_data(3, 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+CALL manipulate_data(3, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 -- Para DELETE, onde o segundo parâmetro é o ID do funcionário que você deseja excluir
 
-
-SELECT Dname, COUNT(*) AS num_people
-FROM employee e
-JOIN department d ON e.Dno = d.Dnumber
-GROUP BY Dname
-ORDER BY num_people DESC
-LIMIT 2;
-
--- Índice utilizado: idx_employee_Dno
--- Tipo de índice: Índice B-tree
-CREATE INDEX idx_employee_Dno ON employee(ativo) USING btree;
-
-SELECT Dname, Dlocation
-FROM department d
-JOIN dept_locations dl ON d.Dnumber = dl.Dnumber;
-
--- Índice utilizado: idx_dept_locations_Dnumber
--- Tipo de índice: Índice B-tree
-CREATE INDEX idx_dept_locations_Dnumber ON dept_locations(ativo) USING btree;
-
-SELECT Dname, COUNT(*) AS num_employees
-FROM employee e
-JOIN department d ON e.Dno = d.Dnumber
-GROUP BY Dname;
-
--- Índice utilizado: idx_employee_Dno
--- Tipo de índice: Índice B-tree
-CREATE INDEX idx_employee_Dno ON employee(ativo) USING btree;
+show indexes from employee;
+show indexes from department;
+show indexes from dept_locations;
+show indexes from project;
+show indexes from works_on;
+show indexes from dependent;
